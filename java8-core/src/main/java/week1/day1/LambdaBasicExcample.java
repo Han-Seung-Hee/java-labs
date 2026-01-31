6f858b19664a35e151354e2de8780e78fb23d34d
Han-Seung-Hee <xorhd1222@gmail.com>
2026-01-31 23:07:05 +0900

    refactor: package structure

--- java8-core/src/main/java/week1/day1/LambdaBasicExcample.java
+++ java8-core/src/main/java/week1/day1/LambdaBasicExcample.java
@@ -1,4 +1,4 @@
-package day1;
+package week1.day1;
 
 public class LambdaBasicExcample {
     public static void main(String[] args) {
--- java8-core/src/main/java/week1/day3/InputLambdaExample.java
+++ java8-core/src/main/java/week1/day3/InputLambdaExample.java
@@ -1,4 +1,4 @@
-package day3;
+package week1.day3;
 
 public class InputLambdaExample {
     interface Printer {
